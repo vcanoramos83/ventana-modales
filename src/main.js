43007435fa154832689ac8dfa4949d8929c623d8
Victor Cano <vcanoramos83@gmail.com>
2025-02-04 12:38:02 +0100

    He realizado mas cambios, creo que ya esta terminado

--- src/main.js
+++ src/main.js
@@ -19,4 +19,35 @@ document.querySelector("#app").innerHTML = `
     <div class="overlay hidden"></div>
 `;
 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
 
+console.log(modal, overlay, btnCloseModal, btnsOpenModal);
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+//asociamos la funcion openModal a cada uno de los botones usando foreach
+btnsOpenModal.forEach((btn) => {
+  btn.addEventListener("click", openModal);
+});
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+const handleKeydown = function (e) {
+  console.log(e.key);
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+};
+
+document.addEventListener("keydown", handleKeydown);
